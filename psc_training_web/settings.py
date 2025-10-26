ab50e12c3c356813dd32ebf3cbe15f48448b10e2
ft-mammoo <mammuofficial007@gmail.com>
2025-10-26 12:44:29 +0530

    settings updated fir database

--- psc_training_web/settings.py
+++ psc_training_web/settings.py
@@ -86,13 +86,17 @@ WSGI_APPLICATION = 'psc_training_web.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST', 'localhost'),
-        'PORT': config('DB_PORT', '5432'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+    #'default': {
+    #    'ENGINE': 'django.db.backends.postgresql',
+    #    'NAME': config('DB_NAME'),
+    #    'USER': config('DB_USER'),
+    #    'PASSWORD': config('DB_PASSWORD'),
+    #    'HOST': config('DB_HOST', 'localhost'),
+    #    'PORT': config('DB_PORT', '5432'),
+    #}
 }
 
 # Password validation
