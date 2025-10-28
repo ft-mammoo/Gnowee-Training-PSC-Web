f24a26f05a4e65e1aa35e17b089ca95916508514
ft-mammoo <mammuofficial007@gmail.com>
2025-10-28 14:36:26 +0530

    Added Courses and Enrollments and connected everything

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from utility.models import BaseModel
 
-# Create your models here.
+class Course(BaseModel):
+    STATUS_CHOICES = (
+        ('d', 'Draft'),
+        ('p', 'Published'),
+        ('a', 'Archived'),
+    )
+    title = models.CharField(max_length=150)
+    description = models.TextField(blank=True, null=True)
+    status = models.CharField(
+        max_length=20, 
+        choices=STATUS_CHOICES, 
+        default='d'
+    )
+
+    def __str__(self):
+        return f"{self.id} - {self.title}"
--- students/models.py
+++ students/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from django.core.validators import MinLengthValidator
 from django.contrib.auth import get_user_model
+from utility.models import BaseModel
 
 User = get_user_model()
 
-class Student(models.Model):
+class Student(BaseModel):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
@@ -20,14 +21,24 @@ class Student(models.Model):
     ), default='a')
     profile_picture = models.CharField(max_length=255, blank=True, null=True)
     date_joined = models.DateField()
-    created_by = models.ForeignKey(
-        User, on_delete=models.DO_NOTHING, null=True, blank=True, related_name="student_created"
-    )
-    updated_by = models.ForeignKey(
-        User, on_delete=models.DO_NOTHING, null=True, blank=True, related_name="student_updated"
-    )
-    created_date = models.DateField(auto_now_add=True)
-    updated_date = models.DateField(auto_now=True)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
+    
+class Enrollment(BaseModel):
+    STATUS_CHOICES = (
+        ('a', 'Active'),
+        ('c', 'Completed'),
+        ('d', 'Dropped'),
+    )
+    student = models.ForeignKey(Student, on_delete=models.CASCADE, related_name='enrollments')
+    course = models.ForeignKey('courses.Course', on_delete=models.CASCADE, related_name='enrollments')
+    enrollment_date = models.DateField(auto_now_add=True)
+    status = models.CharField(
+        max_length=1,
+        choices=STATUS_CHOICES,
+        default='a'
+    )
+
+    def __str__(self):
+        return f"Enrollment {self.id}: Student {self.student_id} in Course {self.course_id}"
