1490baa47c41c95219c933acf4b20cf8bdc73d94
ft-mammoo <mammuofficial007@gmail.com>
2025-10-30 13:18:46 +0530

    Tested some Courses and signals samples

--- courses/models.py
+++ courses/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from utility.models import BaseModel
+from django.dispatch import receiver
 
 class Course(BaseModel):
     STATUS_CHOICES = (
@@ -17,3 +18,15 @@ class Course(BaseModel):
 
     def __str__(self):
         return f"{self.id} - {self.title}"
+
+@receiver(models.signals.pre_save, sender=Course)
+def course_pre_save_reciever(sender, instance, **kwargs):
+    print("Course Pre-Save Signal Triggered")
+    instance.description = "Signal Description Update"
+
+@receiver(models.signals.post_save, sender=Course)
+def course_post_save_reciever(sender, instance, created, **kwargs):
+    if created:
+        print("Course Post-Save Signal Triggered - New Course Created")
+    else:
+        print("Course Post-Save Signal Triggered - Course Updated")
--- courses/tests.py
+++ courses/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from courses.models import Course
 
-# Create your tests here.
+User = get_user_model()
+
+class CourseTest(TestCase):
+    def setUp(self):
+        # Create a test admin
+        self.admin = User.objects.create_user(username='TestAdmin', password='1234')
+    
+    def test_signal(self):
+        c1 = Course.objects.create(
+            title='Physics 101',
+            description='',
+            status='d',
+            created_by=self.admin,
+            updated_by=self.admin
+        )
+        # Verify that the description was updated by the pre-save signal
+        self.assertEqual(c1.description, "Signal Description Update")
+
+        # Update the course to trigger post-save signal for update
+        c1.title = 'Advanced Physics 101'
+        c1.description = 'Advanced concepts in Physics.'
+        c1.save()
