380c8b369386081f16b6a41ab166f3dd4749ef63
ft-mammoo <mammuofficial007@gmail.com>
2025-11-01 00:09:03 +0530

    Added Soft Delete Function and Migrated

--- staffs/models.py
+++ staffs/models.py
@@ -1,20 +1,21 @@
 from django.db import models
 from django.contrib.auth import get_user_model
-from utility.models import BaseModel
+from utility.models import BaseModel, SoftDeleteModel
 
 User = get_user_model()
 
-class Teacher(BaseModel):
+class Teacher(SoftDeleteModel):
     GENDER_CHOICES = (
-        ("male", "Male"),
-        ("female", "Female"),
-        ("other", "Other"),
+        ("m", "Male"),
+        ("f", "Female"),
+        ("o", "Other"),
     )
     STATUS_CHOICES = (
-        ("active", "Active"),
-        ("on_leave", "On Leave"),
-        ("resigned", "Resigned"),
-        ("retired", "Retired"),
+        ("a", "Active"),
+        ("i", "Inactive"),
+        ("l", "On Leave"),
+        ("r", "Resigned"),
+        ("t", "Retired"),
     )
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -33,3 +34,107 @@ class Teacher(BaseModel):
 
     def __str__(self):
         return f"{self.first_name} {self.last_name} - ({self.employee_code})"
+
+class Qualification(SoftDeleteModel):
+    STATUS_CHOICES = (
+        ("a", "Active"),
+        ("i", "Inactive"),
+    )
+
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES, default="a")
+
+    def __str__(self):
+        return f"{self.name}"
+
+class UserQualification(SoftDeleteModel):
+    STATUS_CHOICES = (
+        ("a", "Active"),
+        ("i", "Inactive"),
+    )
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    qualification = models.ForeignKey(Qualification, on_delete=models.CASCADE)
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES, default="a")
+
+    def __str__(self):
+        return f"User {self.user} - Qualification {self.qualification}"
+
+class Specialization(SoftDeleteModel):
+    STATUS_CHOICES = (
+        ("a", "Active"),
+        ("i", "Inactive"),
+    )
+
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES, default="a")
+
+    def __str__(self):
+        return f"{self.name}"
+
+class UserSpecialization(SoftDeleteModel):
+    STATUS_CHOICES = (
+        ("a", "Active"),
+        ("i", "Inactive"),
+    )
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    specialization = models.ForeignKey(Specialization, on_delete=models.CASCADE)
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES, default="a")
+
+    def __str__(self):
+        return f"User {self.user} - Specialization {self.specialization}"
+
+class Department(SoftDeleteModel):
+    STATUS_CHOICES = (
+        ("a", "Active"),
+        ("i", "Inactive"),
+    )
+
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES, default="a")
+
+    def __str__(self):
+        return f"{self.name}"
+
+class UserDepartment(SoftDeleteModel):
+    STATUS_CHOICES = (
+        ("a", "Active"),
+        ("i", "Inactive"),
+    )
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES, default="a")
+
+    def __str__(self):
+        return f"User {self.user} - Department {self.department}"
+
+class Designation(SoftDeleteModel):
+    STATUS_CHOICES = (
+        ("a", "Active"),
+        ("i", "Inactive"),
+    )
+
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES, default="a")
+
+    def __str__(self):
+        return f"{self.name}"
+
+class UserDesignation(SoftDeleteModel):
+    STATUS_CHOICES = (
+        ("a", "Active"),
+        ("i", "Inactive"),
+    )
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    designation = models.ForeignKey(Designation, on_delete=models.CASCADE)
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES, default="a")
+
+    def __str__(self):
+        return f"User {self.user} - Designation {self.designation}"
