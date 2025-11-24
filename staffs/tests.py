564ab355863f759a11138fec8905e58a81611751
ft-mammoo <mammuofficial007@gmail.com>
2025-11-24 13:38:07 +0530

    Teacher User Designation Serializer Test

--- staffs/tests.py
+++ staffs/tests.py
@@ -721,3 +721,112 @@ class DesignationModelSerializerTestCase(TestCase):
             print (se.data)
         print(ctx.captured_queries)
         self.assertEqual(len(se.data), 3)
+
+class UserDesignationModelSerializerTestCase(TestCase):
+    def setUp(self):
+        self.u1 = User.objects.create_user(
+            username='George',
+            password='password123',
+        )
+        self.d1 = mod.Designation.objects.create(
+            name = 'Professor',
+            description = 'Senior academic staff member',
+            status = 'a',
+        )
+        self.ud1 = mod.UserDesignation.objects.create(
+            user = self.u1,
+            designation = self.d1,
+            status = 'a',
+        )
+    def test_model_serializer(self):
+        se = ser.UserDesignationModelSerializer(self.ud1)
+        print(se.data)
+        self.assertEqual(se.data['user'], self.u1.id)
+    def test_create(self):
+        u2 = User.objects.create_user(
+            username='Dravem',
+            password='password123',
+        )
+        d2 = mod.Designation.objects.create(
+            name = 'Assistant Professor',
+            description = 'Junior academic staff member',
+            status = 'a',
+        )
+        data = {
+            'user': u2.id,
+            'designation': d2.id,
+            'status': 'a',
+        }
+        se = ser.UserDesignationModelSerializer(data=data)
+        print(se.is_valid())
+        print(se.errors)
+        self.assertTrue(se.is_valid())
+        se.save()
+        self.assertEqual(mod.UserDesignation.objects.count(), 2)
+        print(se.data)
+    def test_update(self):
+        change = {
+            'status': 'i',
+        }
+        se = ser.UserDesignationModelSerializer(self.ud1, data=change, partial=True)
+        self.assertTrue(se.is_valid())
+        se.save()
+        self.ud1.refresh_from_db()
+        self.assertEqual(self.ud1.status, 'i')
+        print(se.is_valid())
+        print(se.errors)
+        print(se.data)
+
+    def test_listing(self):
+        u2 = User.objects.create_user(
+            username='Helen',
+            password='password123',
+        )
+        u3 = User.objects.create_user(
+            username='Ian',
+            password='password123',
+        )
+        u4 = User.objects.create_user(    
+            username='Jack',
+            password='password123',
+        )
+        u5 = User.objects.create_user(
+            username='Karen',
+            password='password123',
+        )
+        d2 = mod.Designation.objects.create(
+            name = 'Associate Professor',
+            description = 'Mid-level academic staff member',
+            status = 'a',
+        )
+        d3 = mod.Designation.objects.create(
+            name = 'Lecturer',
+            description = 'Entry-level academic staff member',
+            status = 'a',
+        )
+        ud2 = mod.UserDesignation.objects.create(
+            user = u2,
+            designation = d2,
+            status = 'a',
+        )
+        ud3 = mod.UserDesignation.objects.create(
+            user = u3,
+            designation = d3,
+            status = 'a',
+        )
+        ud4 = mod.UserDesignation.objects.create(
+            user = u4,
+            designation = self.d1,
+            status = 'a',
+        )
+        ud5 = mod.UserDesignation.objects.create(
+            user = u5,
+            designation = d2,
+            status = 'a',
+        )
+        qs = mod.UserDesignation.objects.all().select_related('user', 'designation')
+        with CaptureQueriesContext(connection=connection) as ctx:
+            se = ser.UserDesignationModelSerializer(qs, many=True)
+            print (se.data)
+        print(ctx.captured_queries)
+        self.assertEqual(len(se.data), 5)
