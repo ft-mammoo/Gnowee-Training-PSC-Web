ba37d7fb27e92a12e283a308ae2f9565e692dcde
ft-mammoo <mammuofficial007@gmail.com>
2025-11-29 13:55:25 +0530

    readonly fields

--- assessments/serializer.py
+++ assessments/serializer.py
@@ -5,8 +5,15 @@ class AssignmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Assignment
         fields = '__all__'
+        read_only_fields = [
+            'id', 'created_by', 'updated_by', 'created_date', 'updated_date'
+        ]
 
 class SubmissionSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Submission
         fields = '__all__'
+        read_only_fields = [
+            'id', 'created_by', 'updated_by', 'created_date', 'updated_date'
+        ]
+
