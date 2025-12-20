276434238194b587a17c7cd01df73dec4613e1d8
ft-mammoo <mammuofficial007@gmail.com>
2025-12-20 11:22:30 +0530

    Add URLs for Class-based Views in Courses

--- courses/urls.py
+++ courses/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path('fb/courses/', views.course_view, name='course_view'),
     path('fb/courses/<int:pk>/', views.course_detail_view, name='course_detail_fb_view'),
     path('fb/courses/<int:pk>/<str:action>/', views.course_actions_view, name='course_detail_fb_view'),
+    # Class-based Views Urls
+    path('cb/courses/', views.CourseView.as_view(), name='course_view'),
+    path('cb/courses/<int:pk>/', views.CourseDetailedView.as_view(), name='course_detail_cb_view'),
+    path('cb/courses/<int:pk>/<str:action>/', views.CourseActionsView.as_view(), name='course_detail_cb_view'),
 ]
