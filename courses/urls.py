e6b7642423b65d52bdf8aca86eaa0d5781d4870e
ft-mammoo <mammuofficial007@gmail.com>
2025-12-20 11:16:20 +0530

    Added Urls for Function Based Views

--- courses/urls.py
+++ courses/urls.py
@@ -1,4 +1,9 @@
 from django.urls import include, path
 from courses import views
 
-urlpatterns = []
+urlpatterns = [
+    # Function-based Views Urls
+    path('fb/courses/', views.course_view, name='course_view'),
+    path('fb/courses/<int:pk>/', views.course_detail_view, name='course_detail_fb_view'),
+    path('fb/courses/<int:pk>/<str:action>/', views.course_actions_view, name='course_detail_fb_view'),
+]
