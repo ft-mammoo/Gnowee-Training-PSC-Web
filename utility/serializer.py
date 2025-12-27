06ff29da2b1ae55e05b1280a0498225f74a1c10b
ft-mammoo <mammuofficial007@gmail.com>
2025-12-27 13:16:08 +0530

    Fixed a Small Typo Error

--- utility/serializer.py
+++ utility/serializer.py
@@ -3,5 +3,5 @@ from rest_framework import serializers
 class BaseSerializer(serializers.ModelSerializer):
     class Meta:
         read_only_fields = (
-            'id', 'created_at', 'updated_at', 'created_by', 'updated_by'
+            'id', 'created_date', 'updated_date', 'created_by', 'updated_by'
         )
