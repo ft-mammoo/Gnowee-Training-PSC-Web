46a0fdb56a24d69da15737715a5c1c516b7404e4
ft-mammoo <mammuofficial007@gmail.com>
2025-12-27 13:17:40 +0530

    Removed Extra Serializer and Its Test

--- students/serializer.py
+++ students/serializer.py
@@ -5,41 +5,6 @@ from utility.models import User
 from students import models
 from utility.serializer import BaseSerializer
 
-class StudentSerializer(serializers.Serializer):
-    GENDER_CHOICES = (
-        ('m', 'Male'),
-        ('f', 'Female'),
-        ('o', 'Other'),
-    )
-    STATUS_CHOICES = (
-        ('a', 'Active'),
-        ('i', 'Inactive'),
-        ('s', 'Suspended'),
-        ('g', 'Graduated'),
-        ('w', 'Withdrawn'),
-    )
-    id = serializers.IntegerField(read_only=True)
-    user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())  
-    first_name = serializers.CharField(max_length=100) 
-    last_name = serializers.CharField(max_length=100)
-    date_of_birth = serializers.DateField(required = False, allow_null=True)
-    gender = serializers.ChoiceField(choices=GENDER_CHOICES)
-    contact_number = serializers.CharField(max_length=10)
-    emergency_contact_name = serializers.CharField(max_length=100)
-    emergency_contact_number = serializers.CharField(max_length=10)
-    status = serializers.ChoiceField(choices=STATUS_CHOICES, default='a')
-    profile_picture = serializers.CharField(max_length=255, required = False, allow_null = True)
-    date_joined = serializers.DateField()
-
-    def create(self, validated_data):
-        return models.Student.objects.create(**validated_data)
-    
-    def update(self, instance, validated_data):
-        for k,v in validated_data.items():
-            setattr(instance, k, v)
-        instance.save()
-        return instance
-    
 class StudentModelSerializer(BaseSerializer):
     age = serializers.SerializerMethodField()
     class Meta(BaseSerializer.Meta):
