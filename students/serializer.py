4cfc2cdad2e00caf8f99bc739a79755cefe94527
ft-mammoo <mammuofficial007@gmail.com>
2026-04-01 23:45:38 +0530

    `Added dynamic field filtering to StudentSerializer`

--- students/serializer.py
+++ students/serializer.py
@@ -21,13 +21,21 @@ class StudentSerializer(BaseSerializer):
         student = models.Student.objects.create(**validated_data)
         return student
     def __init__(self, *args, **kwargs):
+        allowed_list = kwargs.pop('fields', None)
         super().__init__(*args, **kwargs)
         request = self.context.get('request')
         if request is not None:
             if request.query_params.get('courses'):
                 self.fields['courses'] = CourseSerializer(many=True, read_only=True)
+                if allowed_list is not None:
+                    allowed_list.append('courses')
             else:
                 self.fields.pop('courses', None)
+        if allowed_list is not None:
+            allowed = set(allowed_list)
+            existing = set(self.fields.keys())
+            for field in existing - allowed:
+                self.fields.pop(field)
     def get_age(self, instance):
         if instance.date_of_birth is None:
             return None
