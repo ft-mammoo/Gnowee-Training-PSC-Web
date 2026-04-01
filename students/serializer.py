ed732ec43a8931f99f0d97cdd18f1e4b1faf87f2
ft-mammoo <mammuofficial007@gmail.com>
2026-04-01 21:58:28 +0530

    `Updated UserSerializer and StudentSerializer to use PrimaryKeyRelatedField and SerializerMethodField`

--- students/serializer.py
+++ students/serializer.py
@@ -11,26 +11,26 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ['id', 'username']
 
 class StudentSerializer(BaseSerializer):
-    user = UserSerializer()
+    user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
     age = serializers.SerializerMethodField()
     class Meta(BaseSerializer.Meta):
         model = models.Student
         fields = '__all__'
 
     def create(self, validated_data):
-        user_data = validated_data.pop('user')
-        user = User.objects.create(**user_data)
-        student = models.Student.objects.create(user=user, **validated_data)
+        student = models.Student.objects.create(**validated_data)
         return student
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         request = self.context.get('request')
         if request is not None:
             if request.query_params.get('courses'):
-                self.fields['courses'] = CourseSerializer(many=True)
+                self.fields['courses'] = CourseSerializer(many=True, read_only=True)
             else:
                 self.fields.pop('courses', None)
     def get_age(self, instance):
+        if instance.date_of_birth is None:
+            return None
         return date.today().year - instance.date_of_birth.year
 
 class StudentEnrollmentModelSerializer(BaseSerializer):
