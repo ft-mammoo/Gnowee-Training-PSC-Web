7a1aa9c16ebc1e2cb8385c63e11883a80ed128b7
ft-mammoo <mammuofficial007@gmail.com>
2026-04-05 22:43:50 +0530

    `Added enrollment route to the students module URL configuration`

--- students/urls.py
+++ students/urls.py
@@ -4,6 +4,7 @@ from rest_framework.routers import SimpleRouter
 
 router = SimpleRouter()
 router.register('students', views.StudentViewSet, basename='student')
+router.register('enrollments', views.StudentEnrollmentViewSet, basename='enrollment')
 
 urlpatterns = [
     path('', include(router.urls)),
