4d394f3b7e1a5e270855a11f495ee5594a65d21b
ft-mammoo <mammuofficial007@gmail.com>
2026-04-06 00:03:48 +0530

    'Fixed student/student/ to student/`

--- students/urls.py
+++ students/urls.py
@@ -3,7 +3,7 @@ from students import views
 from rest_framework.routers import SimpleRouter
 
 router = SimpleRouter()
-router.register('students', views.StudentViewSet, basename='student')
+router.register('', views.StudentViewSet, basename='student')
 router.register('enrollments', views.StudentEnrollmentViewSet, basename='enrollment')
 
 urlpatterns = [
