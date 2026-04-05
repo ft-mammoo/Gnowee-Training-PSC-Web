4b5931707f166e60583c4bce8c4f647f0c766129
ft-mammoo <mammuofficial007@gmail.com>
2026-04-05 20:37:42 +0530

    `BaseViewPagination Set page_size to 20`

--- utility/views.py
+++ utility/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from rest_framework.pagination import PageNumberPagination
 
 class BaseViewPagination(PageNumberPagination):
-    page_size = 5
+    page_size = 20
