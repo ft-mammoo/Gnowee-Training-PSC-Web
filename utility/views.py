21803f82822acbcad573c0719d51670dcfcb7c34
ft-mammoo <mammuofficial007@gmail.com>
2026-04-07 00:04:44 +0530

    `Refactor BaseViewPagination to include page_size_query_param and max_page_size attributes`

--- utility/views.py
+++ utility/views.py
@@ -3,6 +3,8 @@ from rest_framework.pagination import PageNumberPagination
 
 class BaseViewPagination(PageNumberPagination):
     page_size = 20
+    page_size_query_param = 'page_size'
+    max_page_size = 100
 
 class EnrollmentViewPagination(BaseViewPagination):
     page_size = 50
