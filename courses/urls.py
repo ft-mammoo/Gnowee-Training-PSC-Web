9dbf86305d488346219688936b2ba8d135740358
ft-mammoo <mammuofficial007@gmail.com>
2026-04-15 14:26:26 +0530

    Update URL routing for courses module to simplify access paths

--- courses/urls.py
+++ courses/urls.py
@@ -4,20 +4,7 @@ from rest_framework.routers import SimpleRouter
 
 router = SimpleRouter()
 router.register('courses', views.CourseViewSet, basename='course')
-router.register('courses-mixin', views.CourseMixinViewSet, basename='course-mixin')
 
 urlpatterns = [
-    # Function-based Views Urls
-    path('fb/courses/', views.course_view, name='course_view'),
-    path('fb/courses/<int:pk>/', views.course_detail_view, name='course_detail_fb_view'),
-    path('fb/courses/<int:pk>/<str:action>/', views.course_actions_view, name='course_detail_fb_view'),
-    # Class-based Views Urls
-    path('cb/courses/', views.CourseView.as_view(), name='course_view'),
-    path('cb/courses/<int:pk>/', views.CourseDetailedView.as_view(), name='course_detail_cb_view'),
-    path('cb/courses/<int:pk>/<str:action>/', views.CourseActionsView.as_view(), name='course_detail_cb_view'),
-    # Generic Class-based Views Urls
-    path('gen/courses/', views.CourseGenericView.as_view(), name='course_generic_view'),
-    path('gen/courses/<int:pk>/', views.CourseDetailGenericView.as_view(), name='course_detail_generic_view'),
-    # ViewSet Urls
     path('', include(router.urls)),
 ]
