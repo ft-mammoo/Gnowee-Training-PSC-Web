9378477b4b2c908c0bf31d550b10d129c3d244b3
ft-mammoo <mammuofficial007@gmail.com>
2026-06-22 18:45:20 +0530

    fix(assessments): enforce submission uniqueness and optimize db indexes
    Added a UniqueConstraint to the Submission model to prevent duplicate active submissions per assignment. Added db_index=True to critical date and status fields across assessment models to optimize query performance.

--- assessments/models.py
+++ assessments/models.py
@@ -1,44 +1,62 @@
-from django.db import models
 from django.contrib.auth import get_user_model
-from utility.models import BaseModel, SoftDeleteModel
+from django.db import models
+
+from utility.models import SoftDeleteModel
 
 User = get_user_model()
 
+
 class Assignment(SoftDeleteModel):
-    course = models.ForeignKey('courses.Course', on_delete=models.CASCADE, related_name='assignments')
-    teacher = models.ForeignKey('staffs.Teacher', on_delete=models.SET_NULL, null=True, blank=True)
+    course = models.ForeignKey("courses.Course", on_delete=models.CASCADE, related_name="assignments")
+    teacher = models.ForeignKey("staffs.Teacher", on_delete=models.SET_NULL, null=True, blank=True)
     title = models.CharField(max_length=150)
     description = models.TextField(blank=True, null=True)
-    due_date = models.DateField(null=True, blank=True)
+    due_date = models.DateField(null=True, blank=True, db_index=True)
 
     def __str__(self):
         return f"{self.title} - (Course_ID:{self.course_id})"
-    
 
-#Submition of assignment by students
+
+# Submition of assignment by students
 class Submission(SoftDeleteModel):
     class SubmissionStatus(models.TextChoices):
         SUBMITTED = "s", "Submitted"
         LATE = "l", "Late"
         GRADED = "g", "Graded"
-    assignment = models.ForeignKey('Assignment', on_delete=models.CASCADE)
-    student = models.ForeignKey('students.Student', on_delete=models.CASCADE)
+
+    assignment = models.ForeignKey("Assignment", on_delete=models.CASCADE)
+    student = models.ForeignKey("students.Student", on_delete=models.CASCADE)
     file_url = models.CharField(max_length=255)
-    submitted_date = models.DateTimeField(auto_now_add=True)
-    status = models.CharField(max_length=1, choices=SubmissionStatus.choices, default=SubmissionStatus.SUBMITTED)
+    submitted_date = models.DateTimeField(auto_now_add=True, db_index=True)
+    status = models.CharField(
+        max_length=1, choices=SubmissionStatus.choices, default=SubmissionStatus.SUBMITTED, db_index=True
+    )
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["assignment", "student"],
+                condition=~models.Q(status="i"),  # Only enforce uniqueness for active records
+                name="unique_active_assignment_submission",
+            )
+        ]
 
     def __str__(self):
         return f"Submission {self.id}: Assignment {self.assignment_id} by Student {self.student_id}"
-    
-#Grading of assignment by teacher
+
+
+# Grading of assignment by teacher
 class SubmissionGrade(SoftDeleteModel):
-    submission = models.ForeignKey('Submission', on_delete=models.CASCADE)
+    submission = models.ForeignKey("Submission", on_delete=models.CASCADE)
     grade = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
-    graded_by = models.ForeignKey('staffs.Teacher', on_delete=models.DO_NOTHING, null=True, blank=True)
+    graded_by = models.ForeignKey("staffs.Teacher", on_delete=models.DO_NOTHING, null=True, blank=True)
     feedback = models.TextField(blank=True, null=True)
 
     def __str__(self):
-        return f"Grading {self.id}: Grade {self.grade} for Submission {self.submission_id} by Teacher {self.graded_by_id}"
+        return (
+            f"Grading {self.id}: Grade {self.grade} for Submission {self.submission_id} by Teacher {self.graded_by_id}"
+        )
+
 
 class QuestionCategories(SoftDeleteModel):
     name = models.CharField(max_length=255)
@@ -47,33 +65,37 @@ class QuestionCategories(SoftDeleteModel):
     def __str__(self):
         return f"{self.name}"
 
+
 class Exams(SoftDeleteModel):
-    course = models.ForeignKey('courses.Course', on_delete=models.CASCADE)
+    course = models.ForeignKey("courses.Course", on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
-    duration = models.DurationField(null=True, blank=True) # Duration Timer
-    start_time = models.DateTimeField() # Window Open
-    end_time = models.DateTimeField() # Window Close
+    duration = models.DurationField(null=True, blank=True)  # Duration Timer
+    start_time = models.DateTimeField(db_index=True)
+    end_time = models.DateTimeField(db_index=True)
     total_marks = models.IntegerField()
 
     def __str__(self):
         return f"{self.title} - (Course_ID:{self.course_id})"
 
+
 class ExamQuestions(SoftDeleteModel):
     class QuestionType(models.TextChoices):
-        SINGLE = 's', 'Single'
-        MULTIPLE = 'm', 'Multiple'
-        TEXT = 't', 'Text'
-    category = models.ForeignKey('QuestionCategories', on_delete=models.SET_NULL, null=True, blank=True)
+        SINGLE = "s", "Single"
+        MULTIPLE = "m", "Multiple"
+        TEXT = "t", "Text"
+
+    category = models.ForeignKey("QuestionCategories", on_delete=models.SET_NULL, null=True, blank=True)
     question_text = models.TextField()
     question_type = models.CharField(max_length=20, choices=QuestionType.choices)
     marks = models.DecimalField(max_digits=5, decimal_places=2)
 
     def __str__(self):
         return f"Question {self.id}: {self.question_text[:30]}..."
-    
+
+
 class QuestionOptions(SoftDeleteModel):
-    question = models.ForeignKey('ExamQuestions', on_delete=models.CASCADE, related_name='options')
+    question = models.ForeignKey("ExamQuestions", on_delete=models.CASCADE, related_name="options")
     option_code = models.CharField(max_length=4)
     option_text = models.TextField()
     is_correct = models.BooleanField(default=False)
@@ -81,76 +103,81 @@ class QuestionOptions(SoftDeleteModel):
     def __str__(self):
         return f"Option {self.option_code} for Question {self.question_id}"
 
+
 class ExamQuestionsMapping(SoftDeleteModel):
-    exam = models.ForeignKey('Exams', on_delete=models.CASCADE, related_name='exam_questions')
-    question = models.ForeignKey('ExamQuestions', on_delete=models.CASCADE)
+    exam = models.ForeignKey("Exams", on_delete=models.CASCADE, related_name="exam_questions")
+    question = models.ForeignKey("ExamQuestions", on_delete=models.CASCADE)
 
     class Meta:
         constraints = [
             models.UniqueConstraint(
-                fields=['exam', 'question'],
-                condition=models.Q(status='a'), # Only enforce uniqueness for active records
-                name='unique_exam_question_mapping'
+                fields=["exam", "question"],
+                condition=models.Q(status="a"),  # Only enforce uniqueness for active records
+                name="unique_exam_question_mapping",
             )
         ]
 
     def __str__(self):
         return f"Exam {self.exam_id} - Question {self.question_id}"
 
+
 class ExamSubmissions(SoftDeleteModel):
-    exam = models.ForeignKey('Exams', on_delete=models.CASCADE)
-    student = models.ForeignKey('students.Student', on_delete=models.CASCADE)
-    submission_time = models.DateTimeField(auto_now_add=True)
-    
+    exam = models.ForeignKey("Exams", on_delete=models.CASCADE)
+    student = models.ForeignKey("students.Student", on_delete=models.CASCADE)
+    submission_time = models.DateTimeField(auto_now_add=True, db_index=True)
+
     class Meta:
         constraints = [
             models.UniqueConstraint(
-                fields=['exam', 'student'],
-                condition=models.Q(status='a'), # Only enforce uniqueness for active records
-                name='unique_exam_student_submission'
+                fields=["exam", "student"],
+                condition=models.Q(status="a"),  # Only enforce uniqueness for active records
+                name="unique_exam_student_submission",
             )
         ]
 
     def __str__(self):
         return f"Exam Submission {self.id}: Exam {self.exam_id} by Student {self.student_id}"
 
+
 class ExamAnswers(SoftDeleteModel):
-    exam_submission = models.ForeignKey('ExamSubmissions', on_delete=models.CASCADE, related_name='answers')
-    question = models.ForeignKey('ExamQuestions', on_delete=models.CASCADE)
+    exam_submission = models.ForeignKey("ExamSubmissions", on_delete=models.CASCADE, related_name="answers")
+    question = models.ForeignKey("ExamQuestions", on_delete=models.CASCADE)
     answer_text = models.TextField(blank=True, null=True)
 
     class Meta:
         constraints = [
             models.UniqueConstraint(
-                fields=['exam_submission', 'question'],
-                condition=models.Q(status='a'), # Only enforce uniqueness for active records
-                name='unique_active_exam_answer'
+                fields=["exam_submission", "question"],
+                condition=models.Q(status="a"),  # Only enforce uniqueness for active records
+                name="unique_active_exam_answer",
             )
         ]
 
     def __str__(self):
         return f"Answer {self.id} for Submission {self.exam_submission_id} - Question {self.question_id}"
 
+
 class ExamAnswerOptions(SoftDeleteModel):
-    answer = models.ForeignKey('ExamAnswers', on_delete=models.CASCADE)
-    option = models.ForeignKey('QuestionOptions', on_delete=models.CASCADE)
+    answer = models.ForeignKey("ExamAnswers", on_delete=models.CASCADE)
+    option = models.ForeignKey("QuestionOptions", on_delete=models.CASCADE)
 
     class Meta:
         constraints = [
             models.UniqueConstraint(
-                fields=['answer', 'option'],
-                condition=models.Q(status='a'), # Only enforce uniqueness for active records
-                name='unique_answer_option'
+                fields=["answer", "option"],
+                condition=models.Q(status="a"),  # Only enforce uniqueness for active records
+                name="unique_answer_option",
             )
         ]
 
     def __str__(self):
         return f"Selected Option {self.option_id} for Answer {self.answer_id}"
 
+
 class ExamReviews(SoftDeleteModel):
-    exam_submission = models.ForeignKey('ExamSubmissions', on_delete=models.CASCADE)
+    exam_submission = models.ForeignKey("ExamSubmissions", on_delete=models.CASCADE)
     score = models.DecimalField(max_digits=5, decimal_places=2)
-    graded_by = models.ForeignKey('staffs.Teacher', on_delete=models.DO_NOTHING)
+    graded_by = models.ForeignKey("staffs.Teacher", on_delete=models.DO_NOTHING)
     feedback = models.TextField(blank=True, null=True)
 
     def __str__(self):
